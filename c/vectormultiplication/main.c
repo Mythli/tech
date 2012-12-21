3ebc8de1422d384e5eb346f9c79fed580773b105
Mythli <tobiasdev@live.de>
2012-12-21 20:19:16 +0100

    array index calculation

--- c/vectormultiplication/main.c
+++ c/vectormultiplication/main.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 
-void MultiplicateMatrix(int* LeftMatrix, int LeftMatrixWidth, int LeftMatrixHeight, int* RightMatrix, int RightMatrixWidth, int RightMatrixHeight, int* OutMatrix)
-{
+void MultiplicateMatrix(int* LeftMatrix, int LeftMatrixWidth, int LeftMatrixHeight, int* RightMatrix, int RightMatrixWidth, int RightMatrixHeight, int* OutMatrix) {
+
+    int matrixWidth = LeftMatrixWidth;
+    int matrixHeight = LeftMatrixHeight;
+
+    if(RightMatrixWidth > matrixWidth) {
+        matrixWidth = RightMatrixWidth;
+    }
+
+    if(RightMatrixHeight > matrixHeight) {
+        matrixHeight = RightMatrixHeight;
+    }
+
+    int i, j;
+    for(i = 0; i < LeftMatrixWidth; i++) {
+        for(j = 0; j < LeftMatrixHeight; j++) {
+            int arrayIndex = i*LeftMatrixHeight+j;
+            printf("to confuse the bitches i: %d j: %d val: %d\n", i, j, LeftMatrix[arrayIndex]);
+        }
+    }
+}
+
+void DisplayMatrix(int* Matrix, int MatrixWidth, int MatrixHeight) {
 
 }
 
-void DisplayMatrix(int* Matrix, int MatrixWidth, int MatrixHeight)
-{
+int main(void) {
 
-}
-
-int main(void)
-{
-
-    int leftMatrix[5][10] = { {5, 6, 7, 3, 7, 8, 1, 2, 11}, {5, 6, 7, 3, 7, 8, 1, 2, 11}, {5, 6, 7, 3, 7, 8, 1, 2, 11}, {5, 6, 7, 3, 7, 8, 1, 2, 11}, {5, 6, 7, 3, 7, 8, 1, 2, 11} };
+    int leftMatrix[5][10] = { {5, 6, 7, 3, 7, 8, 1, 2, 11, 21}, {5, 6, 7, 3, 7, 8, 1, 2, 11, 21}, {5, 6, 7, 3, 7, 8, 1, 2, 11, 21}, {5, 6, 7, 3, 7, 8, 1, 2, 11, 21}, {5, 6, 7, 3, 7, 8, 1, 2, 11, 21} };
     int leftMatrixWidth = 5;
     int leftMatrixHeight = 10;
 
-    int rightMatrix[5][10] = { {5, 6, 7, 3, 7, 8, 1, 2, 11}, {5, 6, 7, 3, 7, 8, 1, 2, 11}, {5, 6, 7, 3, 7, 8, 1, 2, 11}, {5, 6, 7, 3, 7, 8, 1, 2, 11}, {5, 6, 7, 3, 7, 8, 1, 2, 11} };
+    int rightMatrix[5][10] = { {5, 6, 7, 3, 7, 8, 1, 2, 11, 21}, {5, 6, 7, 3, 7, 8, 1, 2, 11, 21}, {5, 6, 7, 3, 7, 8, 1, 2, 11, 21}, {5, 6, 7, 3, 7, 8, 1, 2, 11, 21}, {5, 6, 7, 3, 7, 8, 1, 2, 11, 21} };
     int rightMatrixWidth = 5;
     int rightMatrixHeight = 10;
 
@@ -26,7 +41,7 @@ int main(void)
 
 
     MultiplicateMatrix(&leftMatrix, leftMatrixWidth, leftMatrixHeight, &rightMatrix, rightMatrixWidth, rightMatrixHeight, &outputMatrix);
-    DisplayMatrix(&outputMatrix);
+    //DisplayMatrix(&outputMatrix, );
 
 
     return 0;

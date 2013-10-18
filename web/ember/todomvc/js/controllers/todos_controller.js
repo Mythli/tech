3ae4edfb9fb2e7778ce2b9a6992f43701ccf318a
Mythli <github@mythli.net>
2013-10-18 09:28:22 +0200

    converted spaces to tabs

--- web/ember/todomvc/js/controllers/todos_controller.js
+++ web/ember/todomvc/js/controllers/todos_controller.js
@@ -1,28 +1,28 @@
 Todos.TodosController = Ember.ArrayController.extend({
-    actions: {
-        createTodo: function () {
-            // Get the todo title set by the "New Todo" text field
-            var title = this.get('newTitle');
-            if (!title.trim()) { return; }
+	actions: {
+		createTodo: function () {
+			// Get the todo title set by the "New Todo" text field
+			var title = this.get('newTitle');
+			if (!title.trim()) { return; }
 
-            // Create the new Todo model
-            var todo = this.store.createRecord('todo', {
-                title: title,
-                isCompleted: false
-            });
+			// Create the new Todo model
+			var todo = this.store.createRecord('todo', {
+				title: title,
+				isCompleted: false
+			});
 
-            // Clear the "New Todo" text field
-            this.set('newTitle', '');
+			// Clear the "New Todo" text field
+			this.set('newTitle', '');
 
-            // Save the new model
-            todo.save();
-        },
-	    clearCompleted: function () {
-		    var completed = this.filterProperty('isCompleted', true);
-		    completed.invoke('deleteRecord');
-		    completed.invoke('save');
-	    }
-    },
+			// Save the new model
+			todo.save();
+		},
+		clearCompleted: function () {
+			var completed = this.filterProperty('isCompleted', true);
+			completed.invoke('deleteRecord');
+			completed.invoke('save');
+		}
+	},
 	remaining: function () {
 		return this.filterBy('isCompleted', false).get('length');
 	}.property('@each.isCompleted'),
